be137a23c919ec2af2eed9000f4e4d9592883072
TiagoSB <160522006+Tiago-S-Stefanutto@users.noreply.github.com>
2025-08-25 15:17:03 -0300

    Update Controller.php

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -1,29 +1,29 @@
 <?php
-    namespace Aula_Web\Controller;
 
-    use Aula_Web\Model\Model;
+namespace App\Controller;
 
-    abstract class Controller
+use App\Model\Model;
+
+abstract class Controller
+{
+    final protected static function isProtected() : void
     {
-        final protected static function isProtected() : void
-        {
-            if(!isset($_SESSION['usuario_logado']))
-                header("Location: /login");
-        }
-
-        final protected static function render(string $view, ?Model $model) : void
-        {
-            include VIEWS . "/$view";
-        }
-
-        final protected static function isPost() : bool
-        {
-            return $_SERVER['REQUEST_METHOD'] == "POST";
-        }
-
-        final protected static function redirect(string $route) : void
-        {
-            header("Location: $route");
-        }
+        if(!isset($_SESSION['usuario_logado']))
+            header("Location: /login");
     }
-?>
+
+    final protected static function render(string $view, ?Model $model) : void
+    {
+        include VIEWS . "/$view";
+    }
+
+    final protected static function isPost() : bool
+    {
+        return $_SERVER['REQUEST_METHOD'] == "POST";
+    }
+
+    final protected static function redirect(string $route) : void
+    {
+        header("Location: $route");
+    }
+}
