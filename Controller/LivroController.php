f04a264556c33295829e6324952e7a05ac2a84ca
TiagoSB <160522006+Tiago-S-Stefanutto@users.noreply.github.com>
2025-08-25 15:19:35 -0300

    Update LivroController.php

--- Controller/LivroController.php
+++ Controller/LivroController.php
@@ -1,88 +1,122 @@
 <?php
-    namespace Aula_Web\Controller;
 
-    use Aula_Web\Model\{ Livro, Categoria, Autor };
-    use Exception;
+/**
+ * Declaração de namespaces com sub-namespaces:
+ * https://www.php.net/manual/pt_BR/language.namespaces.nested.php
+ */
+namespace App\Controller;
 
-    final class LivroConrtoller extends Controller
+/**
+ * Definimos aqui que nossa classe precisa incluir uma classe de outro subnamespace
+ * do projeto, no caso a classe Aluno do sub-namespace Model
+ */
+
+use App\Model\{ Categoria, Livro, Autor };
+use Exception;
+
+/**
+ * Classes Controller são responsáveis por processar as requisições do usuário.
+ * Isso significa que toda vez que um usuário chama uma rota, um método (função)
+ * de uma classe Controller é chamado.
+ * O método poderá devolver uma View (fazendo um include), acessar uma Model (para
+ * buscar algo no banco de dados), redirecionar o usuário de rota, ou mesmo,
+ * chamar outra Controller.
+ * Uma classe definida como final não pode ter filhos, ou seja, nenhuma outra classe
+ * pode fazer o extends dela, por exemplo: class Teste extends AlunoController.
+ * Veja mais sobre final aqui: https://www.php.net/manual/pt_BR/language.oop5.final.php
+ */
+final class LivroController extends Controller
+{
+    public static function index() : void
     {
-        public static function index() : void
-        {
-            parent::isProtected();
+        parent::isProtected(); 
 
-            $model = new Livro();
+        $model = new Livro();
+        
+        try {
+            $model->getAllRows();
 
-            try
-            {
-                $model->getAllRows();
-            }
-            catch(Exception $e)
-            {
-                $model->setError("Ocorreu um erro ao buscar os livros:");
-                $model->setError($e->getMessage());
-            }
-
-            parent::render('Livro/lista_livro.php', $model);
+        } catch(Exception $e) {
+            $model->setError("Ocorreu um erro ao buscar os livros:");
+            $model->setError($e->getMessage());
         }
 
-        public static function cadasrto() : void
+        parent::render('Livro/lista_livro.php', $model); 
+    } 
+
+    /**
+     * Declaração de membros de classe estáticos:
+     * https://www.php.net/manual/pt_BR/language.oop5.static.php
+     * Note o tipo de retorno void, ou seja, esse método
+     * é um procedimento e não tem retorno.
+     */
+    public static function cadastro() : void
+    {
+        parent::isProtected(); 
+
+        $model = new Livro();
+
+        echo "Estou onde quero";
+
+        var_dump($model->Id_Autores);
+        
+        try
         {
-            parent::isProtected();
-
-            $model = new Livro();
-
-            try
+            if(parent::isPost())
             {
-                if(parent::isPost())
-                {
-                    $model->Id = !empty($_POST['id']) ? $_POST['id'] : null;
-                    $model->Titulo = $_POST['titulo'];
-                    $model->Id_Categoria = $_POST['id_categoria'];
-                    $model->Isbn = $_POST['isbn'];
-                    $model->Ano = $_POST['ano'];
-                    $model->Editora = $_POST['editora'];
-                    $model->Id_Autores = $_POST['autor'];
-                    $model->save();
+                $model->Id = !empty($_POST['id']) ? $_POST['id'] : null;
+                $model->Titulo = $_POST['titulo'];
+                $model->Id_Categoria = $_POST['id_categoria'];
+                $model->Isbn = $_POST['isbn'];
+                $model->Ano = $_POST['ano'];
+                $model->Editora = $_POST['editora'];
+                $model->Id_Autores = $_POST['autor'];
+                $model->save();
 
-                    parent::redirect("/livro");
-                }
-                else
-                {
-                    if(isset($_GET['id']))
-                    {
-                        $model = $model->getById( (int) $_GET['id']);
-                    }
-                }
-            }
-            catch(Exception $e)
-            {
-                $model->setError($e->getMessage());
-            }
-
-            $model->rows_categorias = new Categoria()->getAllRows();
-            $model->rows_autores = new Autor()->getAllRows();
-
-            parent::render('Livro/form_livro.php', $model);   
-        }
-
-        public static function delete() : void
-        {
-            parent::isProtected();
-
-            $model = new Livro();
-
-            try
-            {
-                $model->delete( (int) $_GET['id']);
                 parent::redirect("/livro");
-            }
-            catch (Exception $e)
-            {
-                $model->setError("Ocorreu um erro ao excluir o livro:");
-                $model->setError($e->getMessage());
+
+            } else {
+    
+                if(isset($_GET['id']))
+                {              
+                    $model = $model->getById( (int) $_GET['id'] );
+                }
             }
 
-            parent::render('Livro/form_livro.php', $model);   
+        } catch(Exception $e) {
+
+            $model->setError($e->getMessage());
         }
+
+        /**
+         * Obtendo a lista de categorias
+         */
+        $model->rows_categorias = new Categoria()->getAllRows();
+
+        /**
+         * Obtendo a lista de autores
+         */
+        $model->rows_autores = new Autor()->getAllRows();
+
+        parent::render('Livro/form_livro.php', $model);        
+    } 
+    
+    public static function delete() : void
+    {
+        parent::isProtected(); 
+
+        $model = new Livro();
+        
+        try 
+        {
+            $model->delete( (int) $_GET['id']);
+            parent::redirect("/livro");
+
+        } catch(Exception $e) {
+            $model->setError("Ocorreu um erro ao excluir o livro:");
+            $model->setError($e->getMessage());
+        } 
+        
+        parent::render('Livro/lista_livro.php', $model);  
     }
-?>
+}
