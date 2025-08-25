3f12f2f52e0079ace2890197f1e320419e9c8dd4
TiagoSB <160522006+Tiago-S-Stefanutto@users.noreply.github.com>
2025-08-25 15:19:13 -0300

    Update InicialController.php

--- Controller/InicialController.php
+++ Controller/InicialController.php
@@ -1,13 +1,13 @@
 <?php
-    namespace Aula_Web\Controller;
+    
+namespace App\Controller;
 
-    final class InicialController extends Controller
+final class InicialController extends Controller
+{
+    public static function index() : void
     {
-        public static function index() : void
-        {
-            parent::isProtected();
+        parent::isProtected();
 
-            include VIEWS . '/Inicial/home.php';
-        }
+        include VIEWS . '/Inicial/home.php';
     }
-?>
+}
