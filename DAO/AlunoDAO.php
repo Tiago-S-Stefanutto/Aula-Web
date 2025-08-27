cb871cbde3ce97494a6b5ea825ded69bfb34bcf9
TiagoSB <160522006+Tiago-S-Stefanutto@users.noreply.github.com>
2025-08-27 10:32:05 -0300

    Update AlunoDAO.php

--- DAO/AlunoDAO.php
+++ DAO/AlunoDAO.php
@@ -13,30 +13,27 @@ final class AlunoDAO extends DAO
 
     public function save(Aluno $model) : Aluno
     {
-        return ($model->$id == null) ? $this->insert($model) :
+        return ($model->Id == null) ? $this->insert($model) : 
             $this->update($model);
     }
 
     public function insert(Aluno $model) : Aluno
     {
-        $sql = "INSERT INTO aluno(nome, ra, curso) VALUES (?, ?, ?) ";
+        $sql = "INSERT INTO aluno (nome, ra, curso) VALUES (?, ?, ?) ";
 
-        $stmt = parent::$conexao->prepare($sql);
-
+        $stmt = parent::$conexao->prepare($sql);        
         $stmt->bindValue(1, $model->Nome);
         $stmt->bindValue(2, $model->RA);
         $stmt->bindValue(3, $model->Curso);
-
         $stmt->execute();
-
-        $model->$id = parent::$conexao->lastInsertId();
-
+        $model->Id = parent::$conexao->lastInsertId();
+        
         return $model;
     }
 
     public function update(Aluno $model) : Aluno
     {
-        $sql = "UPDATE aluno SET nome=?, ra=? curso=? WHERE id=? ";
+        $sql = "UPDATE aluno SET nome=?, ra=?, curso=? WHERE id=? ";
 
         $stmt = parent::$conexao->prepare($sql);
         $stmt->bindValue(1, $model->Nome);
@@ -44,15 +41,49 @@ final class AlunoDAO extends DAO
         $stmt->bindValue(3, $model->Curso);
         $stmt->bindValue(4, $model->Id);
         $stmt->execute();
-
+        
         return $model;
     }
 
     public function selectById(int $id) : ?Aluno
     {
         $sql = "SELECT * FROM aluno WHERE id=? ";
-        
+
+        $stmt = parent::$conexao->prepare($sql);  
+        $stmt->bindValue(1, $id);
+        $stmt->execute();
+
+        return $stmt->fetchObject("App\Model\Aluno");
     }
 
-    
+
+    /**
+     * Método que retorna todas os registros da tabela pessoa no 
+     * banco de dados.
+     */
+    public function select() : array
+    {
+        $sql = "SELECT * FROM aluno ";
+
+        $stmt = parent::$conexao->prepare($sql);  
+        $stmt->execute();
+
+        // Retorna um array com as linhas retornadas da consulta. Observe que
+        // o array é um array de objetos. Os objetos são do tipo stdClass e 
+        // foram criados automaticamente pelo método fetchAll do PDO.
+        return $stmt->fetchAll(DAO::FETCH_CLASS, "App\Model\Aluno");
+    }
+
+    /**
+     * Remove um registro da tabela pessoa do banco de dados.
+     * Note que o método exige um parâmetro $id do tipo inteiro.
+     */
+    public function delete(int $id) : bool
+    {
+        $sql = "DELETE FROM aluno WHERE id=? ";
+
+        $stmt = parent::$conexao->prepare($sql);  
+        $stmt->bindValue(1, $id);
+        return $stmt->execute();
+    }
 }
