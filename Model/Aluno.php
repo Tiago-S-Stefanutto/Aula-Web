7978eaae72509db22c7b59c3f8d846796391d9c6
TiagoSB <160522006+Tiago-S-Stefanutto@users.noreply.github.com>
2025-08-28 14:02:08 -0300

    Update Aluno.php

--- Model/Aluno.php
+++ Model/Aluno.php
@@ -1,70 +1,73 @@
 <?php
-    namespace  Aula_Web\Model;
 
-    use Aula_Web\DAO\AlunoDAO;
-    use Exception;
+namespace App\Model;
 
-    final class Aluno extends Model
+use App\DAO\AlunoDAO;
+use Exception;
+
+final class Aluno extends Model
+{
+    public ?int $Id = null;
+
+    public ?string $Nome
     {
-        public ?int $Id = null;
-
-        public ?string $Nome
+        set
         {
-            set
-            {
-                if(strlen($value) < 4)
-                    throw new Exception("Nome deve ter no mínimo 4 caracteres.");
+            if(strlen($value) < 3)
+                throw new Exception("Nome deve ter no mínimo 3 caracteres.");
 
-                    $this->Nome = $value;
-            }
-
-            get => $this->Nome ?? null;
+            $this->Nome = $value;
         }
 
-        public ?string $RA
-        {
-            set
-            {
-                if(empty($value))
-                    throw new Exception("Preencha o RA.");
-
-                    $this->RA ?? null;
-            }
-
-            get => $this->RA ?? null;
-        }
-
-        public ?string $Curso
-        {
-            set
-            {
-                if(strlen($value) < 4)
-                    throw new Exception("Curso deve ter no mínimo 4 caracteres.");
-
-                    $this->Curso = $value;
-            }
-
-            get => $this->Curso ?? null;
-        }
-
-        function save() : Aluno
-        {
-            return new AlunoDAO()->save($this);
-        }
-
-        function getById(int $id) : ?Aluno
-        {
-            return new AlunoDAO()->selectById($this);
-        }
-
-        function getAllRows() : array
-        {
-            $this->rows = new AlunoDAO()->select();
-        }
-
-        function delete(int $id) : bool
-        {
-            return new AlunoDAO()->delete($id);
-        }
+        get => $this->Nome ?? null;
     }
-?>
+
+    public ?string $RA
+    {
+        set
+        {
+            if(empty($value))
+                throw new Exception("Preencha o RA");
+
+            $this->RA = $value;
+        }
+
+        get => $this->RA ?? null;
+    }
+
+
+    public ?string $Curso
+    {
+        set
+        {
+            if(strlen($value) < 3)
+                throw new Exception("Curso deve ter no mínimo 3 caracteres.");
+
+            $this->Curso = $value;
+        }
+
+        get => $this->Curso ?? null;
+    }
+
+    function save() : Aluno
+    {
+        return new AlunoDAO()->save($this);
+    }
+
+    function getById(int $id) : ?Aluno
+    {
+        return new AlunoDAO()->selectById($id);
+    }
+
+    function getAllRows() : array
+    {
+        $this->rows = new AlunoDAO()->select();
+
+        return $this->rows;
+    }
+
+    function delete(int $id) : bool
+    {
+        return new AlunoDAO()->delete($id);
+    }
+}
